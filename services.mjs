5236317e2ab0ce776f0cd6d513cfcde1b2101834
Anders Houbak Kristiansen <palantus@gmail.com>
2022-06-09 08:58:25 +0200

    fix

--- services.mjs
+++ services.mjs
@@ -13,6 +13,9 @@ export default async () => {
   query.tag("listitem").not(query.prop("orderIdx")).all.sort((a, b) => a._id - b._id).forEach(item => {
     item.orderIdx = nextNum() * 100;
   })
+  query.tag("listitem").prop("orderIdx", null).all.sort((a, b) => a._id - b._id).forEach(item => {
+    item.orderIdx = nextNum() * 100;
+  })
 
   return {}
 }
